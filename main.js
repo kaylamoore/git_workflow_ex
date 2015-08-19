16af259db547e62a0af720faa6da2fb8ed85da71
Kayla Moore <kaylamoo@usc.edu>
2015-08-19 16:38:47 -0700

    working tic tac toe no winner

--- main.js
+++ main.js
@@ -1,32 +1,50 @@
-var boxes= document.querySelectorAll(".square");
-    turns = 0;
+var turnCount = 0;
+var boxes = document.querySelectorAll(".box");
+var button = document.querySelector("button")
 
-for (var i = 0; i < boxes.length; i++){
-     boxes[i].addEventListener("click", getPiece);
-  	}
+button.addEventListener("click", resetBoard);
 
-
-
-
-function getPiece(){
-     if(!this.innerHTML){
-     	if(turns % 2 === 0){
-     		this.innerHTML = "x";
-            console.log('x');
-		} else {
-    		this.innerHTML = "o";
-            console.log('o');
-		}
-		turns ++;
-        console.log(turns);
-	}
+for(var i = 0; i < boxes.length; i++){
+    boxes[i].addEventListener("click", takeTurn)
 }
 
-// button.addEventListener("click", resetBoard);
+function takeTurn(){
+    if(!this.innerHTML){
+        if(turnCount % 2 === 0){
+            this.innerHTML = "X";
+        } else {
+            this.innerHTML = "O";
+        }
+        checkWinner()
+        turnCount++;
+    }   
+}
 
-// function resetBoard(){
-//    for(var i = 0; i < boxes.length; i++){
-//     	boxes[i].innerHTML = “”;
-// 	} 
-// }
- 
+function resetBoard(){
+    for(var i = 0; i < boxes.length; i++){
+        boxes[i].innerHTML = ""
+    }
+}
+
+function checkWinner(){
+    console.log("running checkwinner")
+    if(boxes[0].innerHTML === "X" && boxes[1].innerHTML === "X" && boxes[2].innerHTML === "X" ||
+       boxes[3].innerHTML === "X" && boxes[4].innerHTML === "X" && boxes[5].innerHTML === "X" ||
+       boxes[6].innerHTML === "X" && boxes[7].innerHTML === "X" && boxes[8].innerHTML === "X" ||
+       boxes[0].innerHTML === "X" && boxes[3].innerHTML === "X" && boxes[6].innerHTML === "X" ||
+       boxes[1].innerHTML === "X" && boxes[4].innerHTML === "X" && boxes[7].innerHTML === "X" ||
+       boxes[2].innerHTML === "X" && boxes[5].innerHTML === "X" && boxes[8].innerHTML === "X" ||
+       boxes[0].innerHTML === "X" && boxes[4].innerHTML === "X" && boxes[8].innerHTML === "X" ||
+       boxes[2].innerHTML === "X" && boxes[4].innerHTML === "X" && boxes[6].innerHTML === "X") {
+          console.log("X wins")
+      } else if(boxes[0].innerHTML === "O" && boxes[1].innerHTML === "O" && boxes[2].innerHTML === "O" ||
+       boxes[3].innerHTML === "O" && boxes[4].innerHTML === "O" && boxes[5].innerHTML === "O" ||
+       boxes[6].innerHTML === "O" && boxes[7].innerHTML === "O" && boxes[8].innerHTML === "O" ||
+       boxes[0].innerHTML === "O" && boxes[3].innerHTML === "O" && boxes[6].innerHTML === "O" ||
+       boxes[1].innerHTML === "O" && boxes[4].innerHTML === "O" && boxes[7].innerHTML === "O" ||
+       boxes[2].innerHTML === "O" && boxes[5].innerHTML === "O" && boxes[8].innerHTML === "O" ||
+       boxes[0].innerHTML === "O" && boxes[4].innerHTML === "O" && boxes[8].innerHTML === "O" ||
+       boxes[2].innerHTML === "O" && boxes[4].innerHTML === "O" && boxes[6].innerHTML === "O"){
+          console.log("O wins")
+      }
+}
